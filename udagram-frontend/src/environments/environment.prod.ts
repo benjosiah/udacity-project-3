cad8aee0417e0ba3cb40f6debd2e68833bfaa8fb
josiah <benjosiah90@gmail.com>
2022-09-17 23:44:18 +0100

    deployment

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a409c9c522f2d44bc82cff68e74ecf93-773784359.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
